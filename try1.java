4229a328a17f52db5b547d7de5750c4c5bf59d04
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-12-13 06:48:05 -0600

    Add files via upload

---
+++ try1.java
@@ -0,0 +1,15 @@
+public class try1
+{
+ 	public static void main(String[] args)
+	{
+ 		try
+ 			{
+				int[] myNumber={1, 2, 3};
+				System.out.println(myNumber[10]);
+ 			}
+ 		catch(Exception e)
+		 {
+				System.out.println("Something Went Wrong");
+ 		}
+ 	}
+}
