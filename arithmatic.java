90d66d0b2d268c3c82d21ccf7fd61fa887391d8a
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-12-15 10:42:04 -0600

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,13 @@
+class arithmatic
+{
+	public static void main(String arg[])
+	{
+		int a=10;
+		int b=20;
+		System.out.println("Addition:"+(a+b));
+		System.out.println("Subtraction:"+(a-b));
+		System.out.println("Multiplaction:"+(a*b));
+		System.out.println("Division:"+(a/b));
+		System.out.println("Mod:"+(a%b));
+	}
+}
