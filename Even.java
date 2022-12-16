a2b7217ad06b2107ee325a76b5152b2668a45c3d
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-12-16 07:33:55 -0600

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,16 @@
+class Even
+{
+	public static void main(String args[])
+	{
+		int a;
+		a=2;
+		if(a%2==0)
+		{
+			System.out.println("Number is Even");
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+		}
+	}
+}
