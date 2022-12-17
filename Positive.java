5c8963e7fa22e49eaf7da2ad8c922316e83d36d1
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-12-17 09:59:56 -0600

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,16 @@
+class Positive
+{
+	public static void main(String args[])
+	{
+		int n;
+		n=2;
+		if(n>0)
+		{
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Odd Number");
+		}
+	}
+}
