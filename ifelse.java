2c6e31c7a380aa4c92d45023437660df1de9ffa0
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-12-18 10:44:47 -0600

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+class ifelse
+{
+	public static void main (String args[ ])
+	{
+		int  age;
+		age=20;
+		if(age>18)
+		{
+			System.out.print("You Can Vote");
+		}
+		else
+		{
+			System.out.print("You Cannot Vote");
+		}
+	}
+}
