97032863d8b26da39c6f84057da098b6622c2225
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-12-20 10:39:11 -0600

    Add files via upload

---
+++ Switch1.java
@@ -0,0 +1,25 @@
+class Switch1
+{
+	public static void main(String args[])
+	{
+		int Course = 4;
+		switch(Course)
+		{
+			case 1:
+				System.out.println("BCS");
+				break;
+			case 2:
+				System.out.println("BCA");
+				break;
+			case 3:
+				System.out.println("MCS");
+				break;
+			case 4:
+				System.out.println("MCA");
+				break;
+			default :
+				System.out.println("Wrong Course!!");
+		}
+	}
+}
+			
