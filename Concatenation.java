1cb5dbde899cb137257894aa4c8137fb8108c246
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-12-27 11:17:27 -0600

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,9 @@
+class  Concatenation
+{
+	public static void main(String args [ ])
+	{
+		String firstName = "John";
+		String lastName = "Deo";
+		System.out.println(firstName +" " +lastName);
+	}
+}
